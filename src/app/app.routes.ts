268bab3ccd050ab9091c8528390fa45b814988f1
Emmadi Siddharth <emmadisiddharthreddy@gmail.com>
2025-01-19 20:18:27 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectComponent } from './project/project.component';
+import { SkillsComponent } from './skills/skills.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    {
+        path: 'about-component',
+        component: AboutMeComponent
+    },
+    {
+        path: 'project-component',
+        component: ProjectComponent
+    },
+    {
+        path: 'skills-component',
+        component: AppComponent
+    }
+];
